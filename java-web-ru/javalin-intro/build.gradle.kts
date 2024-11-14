600708e5618f44d70fe092c42d47b8d6072071b5
@hexlet/cli <support@hexlet.io>
2024-11-14 11:06:32 +0300

    submit java-web-ru/javalin-intro

--- java-web-ru/javalin-intro/build.gradle.kts
+++ java-web-ru/javalin-intro/build.gradle.kts
@@ -23,7 +23,6 @@ dependencies {
     
     // END
     implementation("io.javalin:javalin:6.1.3")
-    implementation("io.javalin:javalin-rendering:6.1.3")
     implementation("org.slf4j:slf4j-simple:2.0.7")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
